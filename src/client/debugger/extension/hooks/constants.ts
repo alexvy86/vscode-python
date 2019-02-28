8c3837ac2cbb68ffd91a72aee5c87a9e62013dc2
Alex Villarreal <alex@alexvy86.com>
2019-02-28 00:17:54 -0600

    Cleanup

--- src/client/debugger/extension/hooks/constants.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-'use strict';
-
-export enum PTVSDEvents {
-    // Event sent by PTVSD when a child process is launched and ready to be attached to for multi-proc debugging.
-    ChildProcessLaunched = 'ptvsd_subprocess'
-}
